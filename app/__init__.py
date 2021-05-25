81478f291e2fcfe39df02abc23466dd258c4a0d8
davidparker333 <davidparker333@gmail.com>
2021-05-25 17:29:14 -0400

    added flask-cors to handle api requests

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from config import Config
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 login = LoginManager()
 mail = Mail()
+cors = CORS()
 
 
 def create_app(config_class=Config):
@@ -20,6 +22,7 @@ def create_app(config_class=Config):
     migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
+    cors.init_app(app)
 
     login.login_view = 'auth.login'
     login.login_message_category = 'danger'
